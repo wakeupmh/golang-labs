00ce825bde0fb758d5b6996198521ffd48576534
wakeupmh <wakeupmh@gmail.com>
2021-01-14 09:54:00 -0300

    refactor(array): remove redundant test

--- go-with-tests/arrays-slices/sum_test.go
+++ go-with-tests/arrays-slices/sum_test.go
@@ -3,16 +3,6 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("should sum a collection with five numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-		sum := Sum(numbers)
-		expect := 15
-
-		if sum != expect {
-			t.Errorf("expected '%d' but got '%d', given: %d", expect, sum, numbers)
-		}
-	})
-
 	t.Run("should sum a collection with any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		sum := Sum(numbers)
