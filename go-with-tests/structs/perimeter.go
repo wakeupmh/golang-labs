c0083e39753880274fa3ba76e923bdc645566b79
wakeupmh <wakeupmh@gmail.com>
2021-01-21 18:24:23 -0300

    refactor(structs): add interface

--- go-with-tests/structs/perimeter.go
+++ go-with-tests/structs/perimeter.go
@@ -6,6 +6,10 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	width float64
 	height float64
--- go-with-tests/structs/perimeter_test.go
+++ go-with-tests/structs/perimeter_test.go
@@ -13,23 +13,27 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("should return rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		result := rectangle.Area()
-		expect := 72.0
+	verifyArea := func(t *testing.T, shape Shape, expect float64) {
+		t.Helper()
+
+		result := shape.Area()
 
 		if result != expect {
 			t.Errorf("expect '%.2f' but got '%.2f'", expect, result)
 		}
+	}
+
+	t.Run("should return rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		expect := 72.0
+
+		verifyArea(t, rectangle, expect)
 	})
 
 	t.Run("should return circle area", func(t *testing.T) {
 		circle := Circle{10.0}
-		result := circle.Area()
 		expect := 314.1592653589793
 
-		if result != expect {
-			t.Errorf("expect '%.2f' but got '%.2f'", expect, result)
-		}
+		verifyArea(t, circle, expect)
 	})
 }
