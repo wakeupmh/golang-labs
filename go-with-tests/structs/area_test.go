103fe377c899db04ea39deb4519460e424124b6b
wakeupmh <wakeupmh@gmail.com>
2021-07-23 17:55:02 -0300

    test: ensure test is more descriptive

--- go-with-tests/structs/area_test.go
+++ go-with-tests/structs/area_test.go
@@ -4,18 +4,21 @@ import "testing"
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{12, 6}, want: 72},
-		{shape: Circle{10}, want: 314.1592653589793},
-		{shape: Triangle{12, 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
